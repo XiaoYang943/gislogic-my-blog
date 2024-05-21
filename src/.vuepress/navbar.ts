4a27bbc0f9bf262ed015b66b80263b3e99a84ef9
XiaoYang943 <1352253543@qq.com>
2024-05-21 22:43:01 +0800

    refactor: 隐藏gis之外的笔记

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -3,25 +3,25 @@ import { navbar } from "vuepress-theme-hope";
 export default navbar([
   "/",
   {
-    text: "GIS",
+    text: "Blog",
     icon: "edit",
     link: "/docs/GIS/首页",
   },
-  {
-    text: "Web",
-    icon: "edit",
-    link: "/docs/Web/首页",
-  },
-  {
-    text: "Middleware",
-    icon: "edit",
-    link: "/docs/Middleware/首页",
-  },
-  {
-    text: "Language",
-    icon: "edit",
-    link: "/docs/Language/首页",
-  },
+  // {
+  //   text: "Web",
+  //   icon: "edit",
+  //   link: "/docs/Web/首页",
+  // },
+  // {
+  //   text: "Middleware",
+  //   icon: "edit",
+  //   link: "/docs/Middleware/首页",
+  // },
+  // {
+  //   text: "Language",
+  //   icon: "edit",
+  //   link: "/docs/Language/首页",
+  // },
   {
     text: "Project",
     icon: "edit",
--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -2,7 +2,7 @@ import { sidebar } from "vuepress-theme-hope";
 
 export default sidebar({
     "/docs/GIS/": "structure",
-    "/docs/Language/": "structure",
-    "/docs/Middleware/": "structure",
-    "/docs/Web/": "structure",
+    // "/docs/Language/": "structure",
+    // "/docs/Middleware/": "structure",
+    // "/docs/Web/": "structure",
 });

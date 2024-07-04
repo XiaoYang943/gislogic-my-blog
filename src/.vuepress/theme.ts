230d72c6e99d4eb9ae451931ff0caba273c87ced
XiaoYang943 <1352253543@qq.com>
2024-07-04 21:51:01 +0800

    修改配置

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -1,8 +1,6 @@
 import { hopeTheme } from "vuepress-theme-hope";
 import navbar from "./navbar.js";
 import sidebar from "./sidebar.js";
-import { MR_HOPE_AVATAR } from "./logo.js";
-
 export default hopeTheme({
   hostname: "http://gislogic.cn",
   hotReload:true, // 热更新
@@ -10,12 +8,14 @@ export default hopeTheme({
     name: "今儿有劲儿了",
     url: "http://gislogic.cn",
   },
+  pageInfo:[  // 页面信息
+      "Category",
+      "Tag",
+      "Word",
+      "ReadingTime",
+  ],
 
   iconAssets: "fontawesome-with-brands",
-
-  // logo: "https://theme-hope-assets.vuejs.press/logo.svg",
-  //
-  // repo: "vuepress-theme-hope/vuepress-theme-hope",
 
   docsDir: "src",
 
@@ -24,189 +24,45 @@ export default hopeTheme({
   sidebar,
 
   // 页脚
-  // footer: "默认页脚",
   displayFooter: true,
 
   blog: {
     description: "一条咸鱼GISer",
-    intro: "/intro.html",
+    intro: "/intro.html", // 个人介绍
     medias: {
-      // Baidu: "https://example.com",
       BiliBili: "https://space.bilibili.com/398528203/",
-      // Bitbucket: "https://example.com",
-      // Dingding: "https://example.com",
-      // Discord: "https://example.com",
-      // Dribbble: "https://example.com",
       Email: "mailto:1352253543@qq.com",
-      // Evernote: "https://example.com",
-      // Facebook: "https://example.com",
-      // Flipboard: "https://example.com",
-      // Gitee: "https://example.com",
       GitHub: "https://github.com/XiaoYang943",
-      // Gitlab: "https://example.com",
-      // Gmail: "mailto:info@example.com",
-      // Instagram: "https://example.com",
-      // Lark: "https://example.com",
-      // Lines: "https://example.com",
-      // Linkedin: "https://example.com",
-      // Pinterest: "https://example.com",
-      // Pocket: "https://example.com",
-      // QQ: "https://example.com",
-      // Qzone: "https://example.com",
-      // Reddit: "https://example.com",
-      // Rss: "https://example.com",
       Steam: "https://steamcommunity.com/profiles/76561198853138344",
-      // Twitter: "https://example.com",
-      // Wechat: "https://example.com",
-      // Weibo: "https://example.com",
-      // Whatsapp: "https://example.com",
-      // Youtube: "https://example.com",
       Zhihu: "https://www.zhihu.com/people/1352253543",
-      // MrHope: ["https://mrhope.site", MR_HOPE_AVATAR],
     },
   },
-// comment: false,   // 当前页面是否开启评论功能。
-  lastUpdated: false, // 是否显示最后更新时间。
-  // editLinkfalse: false,  // 是否显示编辑链接。
-  contributors: false,  // 是否显示贡献者。
-  encrypt: {
-    config: {
-      "/demo/encrypt.html": ["1234"],
-    },
-  },
-  // 配置主题提供的插件
+  lastUpdated: true, // 最后更新时间
+  editLink: true,  // 编辑链接
+  contributors: false,  // 贡献者
   plugins: {
     blog: true, // 开启博客功能
     searchPro: true,  // 搜索框
     components: {
       components: ["Badge", "VPCard"],
     },
-
-    // 此处开启了很多功能用于演示，你应仅保留用到的功能。
     mdEnhance: {
-      align: true,
-      attrs: true,
-      codetabs: true,
-      component: true,
-      demo: true,
-      figure: true,
-      imgLazyload: true,
-      imgSize: true,
-      include: true,
-      mark: true,
-      stylize: [
-        {
-          matcher: "Recommended",
-          replacer: ({ tag }) => {
-            if (tag === "em")
-              return {
-                tag: "Badge",
-                attrs: { type: "tip" },
-                content: "Recommended",
-              };
-          },
-        },
-      ],
-      sub: true,
-      sup: true,
-      tabs: true,
-      vPre: true,
-
-      // 在启用之前安装 chart.js
-      // chart: true,
-
-      // insert component easily
-
-      // 在启用之前安装 echarts
-      // echarts: true,
-
-      // 在启用之前安装 flowchart.ts
-      // flowchart: true,
-
-      // gfm requires mathjax-full to provide tex support
-      // gfm: true,
-
+      align: true,  // 文本块内容对齐
+      attrs: true,  // 自定义属性
+      codetabs: true, // 代码组
+      component: true,  // 组件支持
+      demo: true, // 代码案例
+      imgLazyload: true,  // 原生方式懒加载页面图片
+      imgSize: true,  // 图片尺寸
+      include: true,  // Markdown 导入
+      mark: true, // 标记 .eg: 空格==高亮显示==空格
+      sub: true,  // 下角标 .eg ~下角标~
+      sup: true,  // 上角标 .eg ^上角标^
+      tabs: true, // 选项卡
       katex: true,  // TEX语法
-
-      // 在启用之前安装 mathjax-full
-      // mathjax: true,
-
       mermaid: true,  // Mermaid 图表
-
-      // playground: {
-      //   presets: ["ts", "vue"],
-      // },
-
-      // 在启用之前安装 reveal.js
-      // revealJs: {
-      //   plugins: ["highlight", "math", "search", "notes", "zoom"],
-      // },
-
-      // 在启用之前安装 @vue/repl
-      // vuePlayground: true,
-
-      // install sandpack-vue3 before enabling it
-      // sandpack: true,
     },
-
-    // 如果你需要 PWA。安装 @vuepress/plugin-pwa 并取消下方注释
-    // pwa: {
-    //   favicon: "/favicon.ico",
-    //   cacheHTML: true,
-    //   cachePic: true,
-    //   appendBase: true,
-    //   apple: {
-    //     icon: "/assets/icon/apple-icon-152.png",
-    //     statusBarColor: "black",
-    //   },
-    //   msTile: {
-    //     image: "/assets/icon/ms-icon-144.png",
-    //     color: "#ffffff",
-    //   },
-    //   manifest: {
-    //     icons: [
-    //       {
-    //         src: "/assets/icon/chrome-mask-512.png",
-    //         sizes: "512x512",
-    //         purpose: "maskable",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "/assets/icon/chrome-mask-192.png",
-    //         sizes: "192x192",
-    //         purpose: "maskable",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "/assets/icon/chrome-512.png",
-    //         sizes: "512x512",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "/assets/icon/chrome-192.png",
-    //         sizes: "192x192",
-    //         type: "image/png",
-    //       },
-    //     ],
-    //     shortcuts: [
-    //       {
-    //         name: "Demo",
-    //         short_name: "Demo",
-    //         url: "/demo/",
-    //         icons: [
-    //           {
-    //             src: "/assets/icon/guide-maskable.png",
-    //             sizes: "192x192",
-    //             purpose: "maskable",
-    //             type: "image/png",
-    //           },
-    //         ],
-    //       },
-    //     ],
-    //   },
-    // },
   },
 },{
-  // 支持自定义Layout
-  custom: true
+  custom: true    // 支持自定义Layout
 });
